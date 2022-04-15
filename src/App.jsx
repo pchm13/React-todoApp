117a82eecc7226eb4973f486aa8e1551e74fb3c4
momo <82991184+pchm13@users.noreply.github.com>
2022-04-15 14:26:22 +0900

    実装準備

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
+import React from "react";
 import "./styles.css";
 
-export default function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>task1</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>task2</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <div>
+            <li>task3</li>
+            <button>戻す</button>
+          </div>
+          <div>
+            <li>task4</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
   );
-}
+};
