4451d30a8c052f334e2669dba8274c121cfe0481
momo <82991184+pchm13@users.noreply.github.com>
2022-04-15 16:11:44 +0900

    todo追加機能実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState(["task1", "task2"]);
   const [completeTodos, setCompleteTodos] = useState(["task3", "task4"]);
+
+  const onchangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAddTodo = (event) => {
+    if (todoText === "") return;
+    setIncompleteTodos([...incompleteTodos, todoText]);
+    setTodoText("");
+  };
 
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onchangeTodoText}
+        />
+        <button onClick={onClickAddTodo}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
