0fbed0fd66f998854f4ce2daf023ff1195a5dec4
momo <82991184+pchm13@users.noreply.github.com>
2022-04-19 11:41:13 +0900

    未完了todoカウント機能

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -11,12 +11,19 @@ const style = {
 
 export const InputTodo = (props) => {
   // 分割代入でpropsの中身を取り出す
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
 
   return (
     <div style={style} className="input-area">
-      <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
-      <button onClick={onClick}>追加</button>
+      <input
+        disabled={disabled}
+        placeholder="TODOを入力"
+        value={todoText}
+        onChange={onChange}
+      />
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
     </div>
   );
 };
